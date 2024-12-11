7f4b22b25e3c5a3f4e218186a44116e8a716113a
karthik bolla <karthik.bolla@multicorewareinc.com>
2024-12-11 10:14:28 +0530

    delete the image after usage

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from PIL import Image, UnidentifiedImageError
 import numpy as np
-from classify import classify  # Import classify function from classify.py
+from classify import classify  
 import os
 
 app = Flask(__name__)
 
-# Configure upload folder
 UPLOAD_FOLDER = 'static/uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
@@ -25,23 +24,22 @@ def predict():
         return redirect(url_for('home', error="No selected file."))
 
     try:
-        # Save the uploaded image
         filepath = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(filepath)
 
-        # Open the image and process it
         img = Image.open(filepath).convert('RGB')
         img_array = np.array(img)
 
-        # Get the prediction
         prediction = classify(img_array)
 
-        # Provide the uploaded image URL to the template
         uploaded_image_url = url_for('static', filename=f'uploads/{file.filename}')
     except UnidentifiedImageError:
         return redirect(url_for('home', error="Invalid image file. Please upload a valid image."))
     except Exception as e:
         return redirect(url_for('home', error="An error occurred during prediction."))
+    finally:
+        if os.path.exists(filepath):
+            os.remove(filepath)
 
     return render_template('index.html', prediction=prediction, uploaded_image_url=uploaded_image_url)
 
